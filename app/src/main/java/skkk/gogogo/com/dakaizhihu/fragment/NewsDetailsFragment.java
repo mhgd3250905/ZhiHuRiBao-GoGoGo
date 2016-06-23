ee9be3c85e2bee9464e9f641decd0eb6b74266c8
ksheng <294851575@qq.com>
2016-06-24 00:23:12 +0800

    什么都没做 完全没有效率  加入了Jsoup 尝试解析出文字显示在TExt上

--- app/src/main/java/skkk/gogogo/com/dakaizhihu/fragment/NewsDetailsFragment.java
+++ app/src/main/java/skkk/gogogo/com/dakaizhihu/fragment/NewsDetailsFragment.java
@@ -7,10 +7,11 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
+import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -19,6 +20,13 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.UnsupportedEncodingException;
 
 import skkk.gogogo.com.dakaizhihu.NewsDetailsGson.NewDetailsData;
 import skkk.gogogo.com.dakaizhihu.R;
@@ -38,7 +46,10 @@ public class NewsDetailsFragment extends Fragment{
     private String url;
     private String getData;
     private NewDetailsData newsDetailsData;
-    private WebView wvNewsDetails;
+    private Document document;
+    private String title;
+    private String html;
+    private TextView tvNewsDetail;
 
     @Nullable
     @Override
@@ -47,12 +58,14 @@ public class NewsDetailsFragment extends Fragment{
         view = inflater.inflate(R.layout.fragment_news_details, container, false);
         mPref=getActivity().getSharedPreferences("config", Context.MODE_PRIVATE);
         url = mPref.getString("url_from_home", "http://www.baidu.com");
-        wvNewsDetails = (WebView) view.findViewById(R.id.wv_news_details);
+        tvNewsDetail= (TextView) view.findViewById(R.id.tv_news_details);
         initData();
 
         return view;
     }
 
+    private String author;
+    private String content;
     private Handler handler=new Handler(){
         @Override
         public void handleMessage(Message msg) {
@@ -60,7 +73,41 @@ public class NewsDetailsFragment extends Fragment{
             java.lang.reflect.Type type = new TypeToken<NewDetailsData>() {
             }.getType();
             newsDetailsData = gson.fromJson(getData, type);
-            wvNewsDetails.loadDataWithBaseURL(null, newsDetailsData.getBody(), "text/html", "UTF-8", null);;
+
+            try {
+                html = new String(newsDetailsData.getBody().getBytes(), "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+
+            document = Jsoup.parse(html);
+            //获取标题
+            Elements elementsByClass = document.getElementsByClass("question-title");
+
+                title = elementsByClass.text();
+
+            Log.d("TAG","title   "+title);
+            //获取作者
+            Elements getAuthor = document.getElementsByClass("author");
+
+                 author= getAuthor.text();
+
+            Log.d("TAG", "author   "+author);
+
+            Elements getContent = document.getElementsByClass("content");
+
+                content=getContent.text();
+            Log.d("TAG", "content   "+content);
+
+            Elements allElements = document.getAllElements();
+
+            StringBuilder sb=new StringBuilder();
+            for(Element element:allElements){
+                sb.append(element.text()+"\r\n");
+            }
+
+            tvNewsDetail.setText(sb.toString());
         }
     };
 
@@ -69,7 +116,15 @@ public class NewsDetailsFragment extends Fragment{
         StringRequest request=new StringRequest(url, new Response.Listener<String>() {
             @Override
             public void onResponse(String s) {
-                getData = s;
+
+                //由于Volley是用的ISO-8859-1编码，这里需要把字符串转换为utf-8编码。
+                //不然会出现乱码。这个乱码跟AndroidStudio中的乱码还不一样。
+
+                try {
+                    getData = new String (s.getBytes("ISO-8859-1"),"utf-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
                 handler.sendEmptyMessage(0);
             }
         }, new Response.ErrorListener() {
